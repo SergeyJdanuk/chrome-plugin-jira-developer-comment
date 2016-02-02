0e70d132e2d2cab83b6239be196c1516107e9cec
Sergii Zhdanyuk <sergii.zhdanyuk@playtech.com>
2016-02-02 13:05:57 +0200

    Converted tabs to spaces

--- src/js/app.js
+++ src/js/app.js
@@ -3,100 +3,100 @@
 
 $(function() {
 
-	var whatWasDone = null;
-	var affectedAreas = null;
-	var mergeRequest = null;
-	var deployedTo = null;
-	var affectedAreasDefaultVal = 'this was the point fix, should not affect anything, related only to this issue.';
-	var background = chrome.extension.getBackgroundPage();
+    var whatWasDone = null;
+    var affectedAreas = null;
+    var mergeRequest = null;
+    var deployedTo = null;
+    var affectedAreasDefaultVal = 'this was the point fix, should not affect anything, related only to this issue.';
+    var background = chrome.extension.getBackgroundPage();
 
-	initialize();
+    initialize();
 
-	$('#submit').on('click', function(e) {
-		var commentSrc = [
-						"{panel:title=Developer's comment|borderColor=#828282|titleBGColor=#61BEE2|bgColor=#E4F7FD} ",
-					    "- *What was done:* "+whatWasDone.val(),
-					    "- *Affected areas:* "+affectedAreas.val(),
-					    "- *MergeRequest:* "+mergeRequest.val(),
-					   	"- *Deployed to:* "+deployedTo.val(),
-					   	"{panel}"
-					   ].join('\\n');
+    $('#submit').on('click', function(e) {
+        var commentSrc = [
+                        "{panel:title=Developer's comment|borderColor=#828282|titleBGColor=#61BEE2|bgColor=#E4F7FD} ",
+                        "- *What was done:* "+whatWasDone.val(),
+                        "- *Affected areas:* "+affectedAreas.val(),
+                        "- *MergeRequest:* "+mergeRequest.val(),
+                        "- *Deployed to:* "+deployedTo.val(),
+                        "{panel}"
+                       ].join('\\n');
 
-		var code = [
-			"var footerCommentButton = document.getElementById('footer-comment-button');",
-			"var comment = document.getElementById('comment');",
-			"if (footerCommentButton && comment) {",
-			"	footerCommentButton.click();",
-			"	comment.value += \""+commentSrc+"\";",
-			"}"].join('\n');
-	
-		chrome.tabs.executeScript(null, {code: code});
+        var code = [
+            "var footerCommentButton = document.getElementById('footer-comment-button');",
+            "var comment = document.getElementById('comment');",
+            "if (footerCommentButton && comment) {",
+            "   footerCommentButton.click();",
+            "   comment.value += \""+commentSrc+"\";",
+            "}"].join('\n');
+    
+        chrome.tabs.executeScript(null, {code: code});
 
-		window.close();
+        window.close();
 
-	});
+    });
 
-	function initialize() {
-		whatWasDone = initializeVar('#what-was-done-input', 'what-was-done');
-		affectedAreas = initializeVar('#affected-areas-input', 'affected-areas');
-		mergeRequest = initializeVar('#merge-request-input', 'merge-request');
-		deployedTo = initializeVar('#deployed-to-input', 'deployed-to');
+    function initialize() {
+        whatWasDone = initializeVar('#what-was-done-input', 'what-was-done');
+        affectedAreas = initializeVar('#affected-areas-input', 'affected-areas');
+        mergeRequest = initializeVar('#merge-request-input', 'merge-request');
+        deployedTo = initializeVar('#deployed-to-input', 'deployed-to');
 
-		fill();
-	}
+        fill();
+    }
 
-	function initializeVar(id, name) {
-		var el = $(id);
+    function initializeVar(id, name) {
+        var el = $(id);
 
-		el.write = function() {
-			write(name, $(this).val());
-		};
-		el.read = function(defaults) {
-			$(this).val(read(name, defaults));
-		};
-		el.update = function(val) {
-			$(this).val(val);
-			el.write();
-		};
-		el.on('change', function() {
-			el.write();
-		});
-		return el;
-	}
+        el.write = function() {
+            write(name, $(this).val());
+        };
+        el.read = function(defaults) {
+            $(this).val(read(name, defaults));
+        };
+        el.update = function(val) {
+            $(this).val(val);
+            el.write();
+        };
+        el.on('change', function() {
+            el.write();
+        });
+        return el;
+    }
 
-	function write(key, val) {
-		localStorage[genKeyName(key)] = val;
-	}
+    function write(key, val) {
+        localStorage[genKeyName(key)] = val;
+    }
 
-	function read(key, defaults) {
-		var val = localStorage[genKeyName(key)];
-		return val ? val : (defaults !== undefined ? defaults : '');
-	}
+    function read(key, defaults) {
+        var val = localStorage[genKeyName(key)];
+        return val ? val : (defaults !== undefined ? defaults : '');
+    }
 
-	function genKeyName(n) {
-		return '__jira_developer_comment_plugin_' + n;
-	}
+    function genKeyName(n) {
+        return '__jira_developer_comment_plugin_' + n;
+    }
 
-	function fill() {
-		whatWasDone.read();
-		affectedAreas.read(affectedAreasDefaultVal);
-		mergeRequest.read();
-		deployedTo.read();
-	}
+    function fill() {
+        whatWasDone.read();
+        affectedAreas.read(affectedAreasDefaultVal);
+        mergeRequest.read();
+        deployedTo.read();
+    }
 
-	function save() {
-		whatWasDone.write();
-		affectedAreas.write();
-		mergeRequest.write();
-		deployedTo.write();
-	}
-	function clear() {
-		whatWasDone.update('');
-		affectedAreas.update(affectedAreasDefaultVal);
-		mergeRequest.update('');
-		deployedTo.update('');
-	}
+    function save() {
+        whatWasDone.write();
+        affectedAreas.write();
+        mergeRequest.write();
+        deployedTo.write();
+    }
+    function clear() {
+        whatWasDone.update('');
+        affectedAreas.update(affectedAreasDefaultVal);
+        mergeRequest.update('');
+        deployedTo.update('');
+    }
 
-	// Hack, because we couldn't receive unload event correct to save data. https://code.google.com/p/chromium/issues/detail?id=31262#c13
-	setInterval(save, 100);
+    // Hack, because we couldn't receive unload event correct to save data. https://code.google.com/p/chromium/issues/detail?id=31262#c13
+    setInterval(save, 100);
 });
